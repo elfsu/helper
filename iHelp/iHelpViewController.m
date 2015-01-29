f328fdf6c9392ea0f37dd9cbce40048f39b1ad81
Hungyu Su <hungyusu@HungyudeMac.local>
2015-01-29 12:13:49 +0800

    button radius

--- iHelp/iHelpViewController.m
+++ iHelp/iHelpViewController.m
@@ -13,15 +13,19 @@
 @end
 
 @implementation iHelpViewController
+//@synthesize reportButton, personalDataButton;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[_reportButton layer] setCornerRadius:10.0f];
+    [[_personalDataButton layer] setCornerRadius:10.0f];
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 /*
